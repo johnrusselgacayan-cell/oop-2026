a7a82aaf1bbc8fd84bf0b7fa8aa1cf3bdcfebf10
cell <johnrussel.gacayan@lorma.edu>
2026-02-04 13:52:36 +0800

    Mission

---
+++ activity3/Mission.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Mission {
+    private String missionTarget;
+    private LocalDate missionDeadline;
+    private int difficulty;
+}
---
+++ activity3/SecretAgent.java
@@ -0,0 +1,63 @@
+import java.time.LocalDateTime;
+
+public class SecretAgent {
+    private String agentId = "";
+    private String codename;
+    private int clearanceLevel;
+    private boolean onMission;
+    private LocalDateTime lastMissionCompletionTime;
+
+    public SecretAgent(){}
+
+    public SecretAgent(String agentId, String codename, int clearanceLevel){
+        this.agentId = agentId;
+        this.codename = codename;
+    
+        onMission = false;
+        lastMissionCompletionTime = null;
+    }
+
+    public String getAgentId(String agentID){
+        return agentId;
+    }
+
+    public String getCodename(String codename){
+        return codename;
+    }
+
+    public int getClearanceLevel(int clearanceLevel){
+        return clearanceLevel;
+    }
+
+    public boolean isOnMission(){
+        return true;
+    }
+
+    public LocalDateTime getLastMissionCompletionTime(){
+        return lastMissionCompletionTime;
+    }
+
+    public void setCodename(String newCodename){
+        this.codename = newCodename;
+    }
+
+    public void setClearanceLevel(int level){
+        this.clearanceLevel = level;
+        if (level >= 1 && level <= 5) {
+        } else {
+            System.out.println("Invalid clearance level.");
+        }
+
+    }
+
+    public void startMission() {
+        onMission = true;
+    }
+
+    public void completeMission() {
+        onMission = false;
+        lastMissionCompletionTime = LocalDateTime.now();
+    }
+
+
+}
